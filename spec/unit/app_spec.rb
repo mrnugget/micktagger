bb027dce9a81491097e5214c1983830c381475ab
Thorsten Ball <mrnugget@gmail.com>
2012-06-29 11:10:38 +0200

    Clean up app_spec.rb

--- spec/unit/app_spec.rb
+++ spec/unit/app_spec.rb
@@ -2,13 +2,15 @@ require 'spec_helper'
 
 describe App do
   describe 'using command-line arguments' do
+    before(:each) do
+      @db = stub(:db).as_null_object
+      DB.stub(new: @db)
+    end
+
     it 'allows to tag a file when called with "[filename] -a [tagname]"' do
       args = %w[spec/fixtures/test_picture.jpg -a cool]
 
-      db = stub
-      DB.should_receive(:new).with(File.expand_path('~/.micktagger.yml')) { db }
-      db.should_receive(:tag_file!).with(File.expand_path('spec/fixtures/test_picture.jpg'), ['cool'])
-      db.should_receive(:save)
+      @db.should_receive(:tag_file!).with(File.expand_path('spec/fixtures/test_picture.jpg'), ['cool'])
 
       App.parse(args)
     end
@@ -16,10 +18,7 @@ describe App do
     it 'allows to remove a tag from a file when called with "[filename] -r [tagname]"' do
       args = %w[spec/fixtures/test_picture.jpg -r cool]
 
-      db = stub
-      DB.should_receive(:new).with(File.expand_path('~/.micktagger.yml')) { db }
-      db.should_receive(:untag_file!).with(File.expand_path('spec/fixtures/test_picture.jpg'), ['cool'])
-      db.should_receive(:save)
+      @db.should_receive(:untag_file!).with(File.expand_path('spec/fixtures/test_picture.jpg'), ['cool'])
 
       App.parse(args)
     end
@@ -27,22 +26,17 @@ describe App do
     it 'shows the tags of a file when called with only filename' do
       args = %w[spec/fixtures/test_picture.jpg]
 
-      db = stub
-      DB.should_receive(:new).with(File.expand_path('~/.micktagger.yml')) { db }
-      db.should_receive(:tags_of_file).with(File.expand_path('spec/fixtures/test_picture.jpg')).and_return(['cool'])
-      db.should_receive(:save)
+      @db.should_receive(:tags_of_file).with(File.expand_path('spec/fixtures/test_picture.jpg')).and_return(['cool'])
 
       App.parse(args)
     end
+
     it 'shows the files associated with a tag when called with "list [tagname]"' do
-      args = %w[spec/fixtures/test_picture.jpg cool]
-      App.parse(args)
+      @db.stub(files_tagged_with: File.expand_path('spec/fixtures/test_picture.jpg'))
 
       args = %w[list cool]
-      db = stub
-      DB.should_receive(:new).with(File.expand_path('~/.micktagger.yml')) { db }
-      db.should_receive(:files_tagged_with).with('cool').and_return(File.expand_path('spec/fixtures/test_picture.jpg'))
-      db.should_receive(:save)
+
+      @db.should_receive(:files_tagged_with).with('cool')
 
       App.parse(args)
     end
