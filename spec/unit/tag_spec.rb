26a926d49a07858b062849b017c4e7c1b2832b87
Thorsten Ball <mrnugget@gmail.com>
2012-06-29 11:17:20 +0200

    Clean up tag_spec.rb

--- spec/unit/tag_spec.rb
+++ spec/unit/tag_spec.rb
@@ -2,25 +2,28 @@ require 'spec_helper'
 
 describe Tag do
 
-  before(:each) do
-    @tag = Tag.new("testing")
+  let(:tag) do
+    Tag.new('testing')
   end
 
-  it "is empty when initialized" do
-    @tag.files.should be_empty
-    @tag.no_files?.should be_true
+  it 'has no files associated with it when initialized' do
+    tag.files.should be_empty
+    tag.no_files?.should be_true
   end
 
-  it "allows to add files" do
-    @tag.add_file("testing.txt")
+  it 'allows to associate files' do
+    tag.add_file('testing.txt')
 
-    @tag.files.should include("testing.txt")
+    tag.files.should include("testing.txt")
   end
 
-  it "allows to remove files" do
-    @tag.stub(files: ["anotherfile.mkv"])
-    @tag.remove_file("testing.txt")
+  it 'allows to remove files' do
+    tag.add_file('the_first_file.txt')
+    tag.add_file('the_second_file.txt')
 
-    @tag.files.should_not include("testing.txt")
+    tag.remove_file('the_first_file.txt')
+
+    tag.files.should_not include('the_first_file.txt')
+    tag.files.count.should eq(1)
   end
 end
